018884c938f6564fac69f778029c7f339c97ae10
gnarmedia <gnarmedia@users.noreply.github.com>
2019-12-11 01:55:49 -0800

    WIP: Revise attributions markup

--- src/components/Attribution/Attribution.jsx
+++ src/components/Attribution/Attribution.jsx
@@ -5,12 +5,9 @@ import "./Attribution.scss";
 function Attribution() {
   return (
     <div className="attributions">
-      <span className="attribution">
-        <a href="https://darksky.net/poweredby/" alt="Powered by Dark Sky">
-          Powered by Dark Sky
-        </a>
-      </span>
-      <a
+      <a href="https://darksky.net/poweredby/" alt="Powered by Dark Sky">
+        Powered by Dark Sky
+      </a> with <a
         href="https://www.amcharts.com/free-animated-svg-weather-icons/"
         alt="Icons by amCharts"
       >
