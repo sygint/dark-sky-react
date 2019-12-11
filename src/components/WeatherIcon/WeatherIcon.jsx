295009012e97b4b3f8c92cd42bb8af69513a8731
gnarmedia <gnarmedia@users.noreply.github.com>
2019-12-11 01:50:45 -0800

    WIP: Fix WeatherIcon component and add svgs

--- src/components/WeatherIcon/WeatherIcon.jsx
+++ src/components/WeatherIcon/WeatherIcon.jsx
@@ -1,15 +1,50 @@
 import React from "react";
 
-import "./WeatherIcon.css";
+import "./WeatherIcon.css"
 
-function WeatherIcon(props) {
+import { ReactComponent as ClearDay } from "./icons/clear-day.svg";
+import { ReactComponent as ClearNight } from "./icons/clear-night.svg";
+import { ReactComponent as PartlyCloudyDay } from "./icons/partly-cloudy-day.svg";
+import { ReactComponent as PartlyCloudyNight } from "./icons/partly-cloudy-night.svg";
+import { ReactComponent as CloudyDay } from "./icons/cloudy-day.svg";
+import { ReactComponent as CloudyNight } from "./icons/cloudy-night.svg";
+import { ReactComponent as Cloudy } from "./icons/cloudy.svg";
+import { ReactComponent as Fog } from "./icons/fog.svg";
+import { ReactComponent as Rain } from "./icons/rain.svg";
+import { ReactComponent as Sleet } from "./icons/sleet.svg";
+import { ReactComponent as Snow } from "./icons/snow.svg";
+
+const icons = {
+  "clear-day": <ClearDay />,
+  "clear-night": <ClearNight />,
+  "partly-cloudy-day": <PartlyCloudyDay />,
+  "partly-cloudy-night": <PartlyCloudyNight />,
+  "cloudy-day": <CloudyDay />,
+  "cloudy-night": <CloudyNight />,
+  cloudy: <Cloudy />,
+  fog: <Fog />,
+  rain: <Rain />,
+  sleet: <Sleet />,
+  snow: <Snow />
+};
+
+function renderWeatherIcon(icon) {
+  if (!icons.hasOwnProperty(icon)) {
+    console.error(`icon "${icon}" not found`);
+    return null;
+  }
+
+  return icons[icon];
+}
+
+function WeatherIcon({ icon }) {
+  console.log(icon)
   return (
     <div
       className="avatar weather-icon"
-      style={{
-        backgroundImage: `url('/images/clear-day.svg')`
-      }}
-    />
+    >
+      { renderWeatherIcon(icon) }
+    </div>
   );
 }
 
